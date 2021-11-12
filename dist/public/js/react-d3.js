32fbda4dfcd05f216299517d749644f560c4b832
koji <robson.koji@gmail.com>
2021-11-12 19:19:29 -0300

    remove console.log

--- dist/public/js/react-d3.js
+++ dist/public/js/react-d3.js
@@ -4174,9 +4174,6 @@ module.exports = createReactClass({
       colorsAccessor = this.props.colorAccessorOrdinal;
     }
 
-    // console.log(props.voronoiStroke)
-    // debugger;
-
     return React.createElement('span', { onMouseLeave: this.onMouseLeave }, React.createElement(Chart, {
       viewBox: this.getViewBox(),
       legend: props.legend,
